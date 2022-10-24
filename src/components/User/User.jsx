137433200dbafc88b79599dd92937c720059b640
gegaenea@yahoo.com <gegaenea@yahoo.com>
2022-10-24 03:16:08 +0300

    Add User Functionality

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -7,23 +7,37 @@ import userStyle from './User.module.css'
 import femaleUser from '../../assets/woman.svg'
 import maleUser from '../../assets/man.svg'
 import userMail from '../../assets/mail.svg'
-import userAge from '../../assets/growing-up-man.svg'
+import manAge from '../../assets/growing-up-man.svg'
+import womanAge from '../../assets/growing-up-woman.svg'
 import userLocation from '../../assets/map.svg'
 import userPhone from '../../assets/phone.svg'
 import userPassword from '../../assets/padlock.svg'
+import UserDisplay from "../UserDisplay/UserDisplay"
 
 
 const User = () => {
   const [user, setUser] = useState("")
-
+  const [addedUser, setAddedUser] = useState([])
+  
   const submit = () => {
     axios.get('https://randomuser.me/api').then((res) => {
-      setUser(res.data.results[0]);
-      
+      setUser(res.data.results);
       console.log(user);
     })
   }
-  useEffect(() => {
+
+const displayHandler = () => {
+  const newUser = [...user, ...addedUser ]
+setAddedUser(newUser)
+
+console.log(addedUser);
+  }
+
+useEffect(() => {
+  displayHandler()
+}, [])
+
+useEffect(() => {
     submit()
     return () => {
       console.log(user);
@@ -36,26 +50,25 @@ return (
 !user ? <div className="wrapper">
   <Button submit={submit} /> 
 </div> : ( 
-<div className="wrapper">
-     <img src={user.picture.large} alt="" />
+<div className={userStyle.wrapper}>
+     <img src={user[0].picture.large} alt="" />
        <div>
-          <h3>{gender}</h3> 
-          <h3>{email}</h3> 
-          <h3>{user.dob.age}</h3>   
-       </div>
-       <div className={userStyle.ismail}>
-        Selam Ismail
+          <h3 className={userStyle.gender} >{user[0].gender}</h3> 
+          <h3 className={userStyle.email} >{user[0].email}</h3> 
+          <h3 className={userStyle.age} >{user[0].dob.age}</h3>   
        </div>
        <div className={userStyle.icons}>
-        {gender === "female" ? <img src={femaleUser} alt="" /> : <img src={maleUser} alt="" />} 
+        {gender === "female" ? <img className={userStyle.avatar} src={femaleUser} alt="" /> : <img className={userStyle.avatar} src={maleUser} alt="" />} 
         <img src={userMail} alt="" />
-        <img src={userAge} alt="" />
+        {gender === "female" ? <img src={womanAge} alt="" /> : <img src={manAge} alt="" />}
         <img src={userLocation} alt="" />
         <img src={userPhone} alt="" />
         <img src={userPassword} alt="" />
        </div>
        <Button submit={submit} /> 
-       
+       <button onClick={displayHandler} className={userStyle.btn}>Add User</button>
+       <UserDisplay displayHandler={displayHandler} addedUser={addedUser} setAddedUser={setAddedUser}/>
+
     </div> 
     
   ) )
---
+++ src/components/UserDisplay/UserDisplay.jsx
@@ -0,0 +1,18 @@
+const UserDisplay = ({addedUser, setAddedUser}) => {
+      
+  return (
+    <div>
+      {addedUser?.map((user) => {
+        console.log(user);
+      return  <div>
+        <h2>{user.gender}</h2>
+        <h2>{user.email}</h2>
+        </div>
+       
+      })}
+    
+    </div>
+  )
+}
+
+export default UserDisplay
