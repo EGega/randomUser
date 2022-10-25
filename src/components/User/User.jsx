08cee57990a2b34910ee43844b30d3b176a0a270
gegaenea@yahoo.com <gegaenea@yahoo.com>
2022-10-25 04:05:13 +0300

    Project Finished

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -2,8 +2,6 @@ import axios, { Axios } from "axios"
 import Button from "../Button/Button"
 import { useEffect, useState } from "react"
 import userStyle from './User.module.css'
-
-
 import femaleUser from '../../assets/woman.svg'
 import maleUser from '../../assets/man.svg'
 import userMail from '../../assets/mail.svg'
@@ -13,12 +11,20 @@ import userLocation from '../../assets/map.svg'
 import userPhone from '../../assets/phone.svg'
 import userPassword from '../../assets/padlock.svg'
 import UserDisplay from "../UserDisplay/UserDisplay"
-import SingleElement from "./SingleElement"
+
 
 
 const User = () => {
   const [user, setUser] = useState("")
   const [addedUser, setAddedUser] = useState([])
+  const [hoveredElement, setHoveredElement] = useState({
+    name: false,
+    email: false,
+    age: false,
+    location: false,
+    phone: false,
+    login: false
+  })
   
   const submit = () => {
     axios.get('https://randomuser.me/api').then((res) => {
@@ -26,7 +32,6 @@ const User = () => {
       console.log(user);
     })
   }
-
 const displayHandler = () => {
   const newUser = [...user, ...addedUser ]
 setAddedUser(newUser)
@@ -41,35 +46,80 @@ useEffect(() => {
     }
   }, [])
 
- const {gender, email} = user
  
-return (
-!user ? <div className="wrapper">
-  <Button submit={submit} /> 
-</div> : ( 
-<div className={userStyle.wrapper}>
-     <img src={user[0].picture.large} className={userStyle.img} alt="" />
-       <div id="displayer-div">
-          <h3 className={userStyle.gender} >{user[0].gender}</h3> 
-          {/* <SingleElement ageDisplayer={ageDisplayer} user={user} emailDisplayer={emailDisplayer} /> */}
-          <h3 className={userStyle.email} >{user[0].email}</h3> 
-          <h3 className={userStyle.age} >{user[0].dob.age}</h3> 
-       </div>
-       <div className={userStyle.icons}>
-        {gender === "female" ? <img className={userStyle.avatar} src={femaleUser} alt="" /> : <img className={userStyle.avatar} src={maleUser} alt="" />} 
-        <img src={userMail} alt="" />
-        {gender === "female" ? <img src={womanAge} alt="" /> : <img src={manAge} alt="" />}
-        <img src={userLocation} alt="" />
-        <img src={userPhone}  alt="" />
-        <img src={userPassword}  alt="" />
-       </div>
-       <Button submit={submit} /> 
-       <button onClick={displayHandler} className={userStyle.btn}>Add User</button>
-       <UserDisplay displayHandler={displayHandler} addedUser={addedUser} setAddedUser={setAddedUser}/>
+ return (
+ !user ? <div className="wrapper">
+   <Button submit={submit} /> 
+ </div> : ( 
+ <div className={userStyle.wrapper}>
+      <img src={user[0].picture.large} className={userStyle.img} alt="" />
+      <div id="displayer-div" className={userStyle.displayer}>
+           <div className="mt-3">
+            {hoveredElement.name &&
+            <><h3>Name</h3>
+            <p>{user[0].name.title} {user[0].name.first} {user[0].name.last} </p></>}
 
-    </div> 
+            {hoveredElement.email &&
+            <><h3>Email</h3>
+            <p>{user[0].email} </p></>}
+
+            {hoveredElement.age &&
+            <><h3>Age</h3>
+            <p>{user[0].dob.age} </p></>}
+
+            {hoveredElement.location &&
+            <><h3>Residency</h3>
+            <p>{user[0].location.city} {user[0].location.country} </p></>}
+           
+            {hoveredElement.phone &&
+            <><h3>Phone</h3>
+            <p>{user[0].phone}</p></>}
+            {hoveredElement.login &&
+            <><h3>Username</h3>
+            <p>{user[0].login.username}</p></>}
+            
+           </div>
+        </div>
+        <div className={userStyle.icons}>
+         {user[0].gender === "female" ? <img src={femaleUser} alt="" className={userStyle.avatar} onMouseEnter={() => {
+          setHoveredElement({...hoveredElement, name:true})}} onMouseLeave={() => {
+            setHoveredElement({...hoveredElement, name:false}) 
+           }}  /> 
+         : <img className={userStyle.avatar} src={maleUser} alt=""  onMouseEnter={() => {
+          setHoveredElement({...hoveredElement, name:true})}} onMouseLeave={() => {
+            setHoveredElement({...hoveredElement, name:false}) 
+           }} />} 
+         <img src={userMail} onMouseEnter={() => {
+          setHoveredElement({...hoveredElement, email:true})}} onMouseLeave={() => {
+            setHoveredElement({...hoveredElement, email:false}) 
+           }}  alt="" />
+         {user[0].gender === "female" ? <img src={womanAge} onMouseEnter={() => {
+          setHoveredElement({...hoveredElement, age:true})}} onMouseLeave={() => {
+            setHoveredElement({...hoveredElement, age:false}) 
+           }}  alt="" /> : <img src={manAge} alt="" onMouseEnter={() => {
+            setHoveredElement({...hoveredElement, age:true})}} onMouseLeave={() => {
+              setHoveredElement({...hoveredElement, age:false}) 
+             }}  />}
+         <img src={userLocation} onMouseEnter={() => {
+          setHoveredElement({...hoveredElement, location:true})}} onMouseLeave={() => {
+            setHoveredElement({...hoveredElement, location:false}) 
+           }}  alt="" />
+         <img src={userPhone}  onMouseEnter={() => {
+          setHoveredElement({...hoveredElement, phone:true})}} onMouseLeave={() => {
+            setHoveredElement({...hoveredElement, phone:false}) 
+           }}  alt="" />
+         <img src={userPassword}  onMouseEnter={() => {
+          setHoveredElement({...hoveredElement, login:true})}} onMouseLeave={() => {
+            setHoveredElement({...hoveredElement, login:false}) 
+           }}  alt="" />
+        </div>
+        <Button submit={submit} /> 
+        <button onClick={displayHandler} className={userStyle.btn}>Add User</button>
+        <UserDisplay displayHandler={displayHandler} addedUser={addedUser}/>
+
+     </div> 
     
-  ) )
+   ) )
 }
 
 export default User
--- src/components/UserDisplay/UserDisplay.jsx
+++ src/components/UserDisplay/UserDisplay.jsx
@@ -1,17 +1,34 @@
-const UserDisplay = ({addedUser, setAddedUser}) => {
+ 
+const UserDisplay = ({addedUser}) => { 
+  console.log(addedUser);
+  if(addedUser.length !== 0) {
+    return (
+      <div>
+        <table className="table mt-5 table-dark">
+    <thead>
+      <tr>
+        <th scope="col">Gender</th>
+        <th scope="col">Email</th>
+        <th scope="col">Name</th>
+      </tr>
+    </thead>
+    <tbody>
+    {addedUser?.map((user) => {
+          console.log(user);
+         return <tr>
+             <td>{user.gender}</td>
+            <td>{user.email}</td>
+            <td>{user.name.title} {user.name.first} {user.name.last}</td>
+          </tr>
+        
+        })}
+        </tbody>
+  </table>
+        
       
-  return (
-    <div>
-      {addedUser?.map((user) => {
-        console.log(user);
-      return  <div>
-        <h2>{user.gender}</h2>
-        <h2>{user.email}</h2>
-        </div>
-      })}
-    
-    </div>
-  )
+      </div>
+    )
+  }    
 }
 
 export default UserDisplay
